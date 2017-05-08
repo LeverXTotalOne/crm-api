fd0fb7e3e996ebe10ddc8b8c4a60fe2b22ea3f44
LEVERX\dzmitry.korshun <dzmitry.korshun@leverx.com>
2017-05-08 14:47:50 +0300

    asset handler

---
+++ BusinessLayer.CohesiveObjects/Interfaces/IAssetHandler.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using SoftwareLicenses.Service.Contracts.Entities;
+
+namespace SoftwareLicenses.Service.Contracts.Interfaces
+{
+	public interface IAssetHandler : ICrudHandler<Asset>
+	{
+		
+	}
+}
